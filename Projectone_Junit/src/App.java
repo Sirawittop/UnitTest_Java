5e6f6074e1bf379d4ac38f7c0b316f7a7dd71cc0
Sirawit Kamchoom <102017994+TopSirawit@users.noreply.github.com>
2023-02-07 11:20:15 +0700

    0

--- Projectone_Junit/src/App.java
+++ Projectone_Junit/src/App.java
@@ -3,9 +3,15 @@ public class App {
         int maxvalue = 0;
         int count = 0;
         for (int i = PeopleList1.length-1;i>= 0;i--){
-            if (PeopleList1[i] > maxvalue){
+            
+            if(PeopleList1[i] != 0){
+                if (PeopleList1[i] > maxvalue){
                 maxvalue = PeopleList1[i];
                 count++;
+                }
+            }else{
+                System.out.println("Value Error Must More Than Zero");
+                break;
             }
         }
         return count;
--- Projectone_Junit/src/TessFindCountPeople.java
+++ Projectone_Junit/src/TessFindCountPeople.java
@@ -1,10 +1,7 @@
 class TessFindCountPeople{
     public static void main(String[] args) {
-        App testetste = new App();
         App testetste2 = new App();
-        int toptop = testetste.FindCountPeople(4,2,0,8,5);
-        int toptop2 = testetste2.FindCountPeople(1,1,1,1,1,1,1,1,1,1);
-        System.out.println(toptop);
+        int toptop2 = testetste2.FindCountPeople(0,0,0,0,0,0,0,0,0,0,0);
         System.out.println(toptop2);
     }
 }
