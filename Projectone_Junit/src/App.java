b65f8bed4bb0168a2e8cd84ea87eae51e5a41a62
Sirawit Kamchoom <102017994+TopSirawit@users.noreply.github.com>
2023-02-08 13:31:33 +0700

    Finally

--- Projectone_Junit/src/App.java
+++ Projectone_Junit/src/App.java
@@ -2,18 +2,18 @@ public class App {
     int FindCountPeople(Integer... PeopleList1){   
         int maxvalue = 0;
         int count = 0;
-        for (int i = PeopleList1.length-1;i>= 0;i--){
+        if(PeopleList1.length == 0){
             
-            if(PeopleList1[i] != 0){
+        }
+        for (int i = PeopleList1.length-1;i>= 0;i--){
+            if (PeopleList1[i] > 0){
                 if (PeopleList1[i] > maxvalue){
                 maxvalue = PeopleList1[i];
                 count++;
                 }
-            }else{
-                System.out.println("Value Error Must More Than Zero");
-                break;
             }
         }
+
         return count;
     }
 }
--- Projectone_Junit/src/TessFindCountPeople.java
+++ Projectone_Junit/src/TessFindCountPeople.java
@@ -1,7 +1,39 @@
-class TessFindCountPeople{
-    public static void main(String[] args) {
-        App testetste2 = new App();
-        int toptop2 = testetste2.FindCountPeople(0,0,0,0,0,0,0,0,0,0,0);
-        System.out.println(toptop2);
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class TessFindCountPeople {
+    @Test
+    public void testFindCountPeople() {
+        App app = new App();
+        int result = app.FindCountPeople(1, 2, 3, 4, 5, 0);
+        assertEquals(1, result);
     }
+    @Test
+    public void testFindCountPeopleWithNegativeNumber() {
+        App app = new App();
+        int result = app.FindCountPeople(1, 2, -3, 4, 5);
+        assertEquals(1, result);
+}
+
+    @Test
+    public void testFindCountPeopleWithZero() {
+        App app = new App();
+        int result = app.FindCountPeople(0, 0, 0, 0, 0);
+        assertEquals(0, result);
+}
+
+    @Test
+    public void testFindCountPeopleWithOneElement() {
+        App app = new App();
+        int result = app.FindCountPeople(5);
+        assertEquals(1, result);
+}
+    @Test
+    public void testFindCountPeopleWithEmptyList() {
+        App app = new App();
+        int result = app.FindCountPeople();
+        assertEquals(0, result);
+}
+
+
 }
